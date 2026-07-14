53916a4cfa775a4691d8915e90204ae2a387bdf2
Charlie Butcosk <incharge@weichbild.com>
2026-07-14 09:19:25 -0400

    Fix lint

--- packages/11ty/_plugins/figures/figureMedia/index.js
+++ packages/11ty/_plugins/figures/figureMedia/index.js
@@ -28,7 +28,6 @@ const logger = chalkFactory('Figures:FigureMedia', 'DEBUG')
  * @property {String} printImage Optional path to an alternate image to use in print
  */
 export default class FigureMedia {
-
   /**
    * @private
    * @property #annotations
